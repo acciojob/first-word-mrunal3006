0cb58a029d9048e01ed9917b723933984f3edd37
mrunal3006 <128721188+mrunal3006@users.noreply.github.com>
2023-12-07 19:40:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+	 // Check if the input string is empty or does not contain any space
+  if (str === '' || !str.includes(' ')) {
+    return str;
+  }
+
+  // Find the index of the first space
+  const spaceIndex = str.indexOf(' ');
+
+  // Return the first word, which is all characters up to the first space
+  return str.substring(0, spaceIndex);
+	
 }
 
 // Do not change the code below
